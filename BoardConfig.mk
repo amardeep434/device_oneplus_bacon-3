e028334a231206a6ffc521e5a7353b6a712d8c1b
InkSlinger666 <kengreenwood420666@gmail.com>
2016-05-23 16:46:57 -0500

    That endif dont belong there...Move that thang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,7 @@ WPA_SUPPLICANT_VERSION           := VER_0_8_X
 # Inherit from QC proprietary
 ifneq ($(QCPATH),)
 -include $(QCPATH)/common/msm8974/BoardConfigVendor.mk
+endif
 
 # Bluetooth
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
@@ -194,7 +195,6 @@ BOARD_USES_QCNE := true
 
 ifeq ($(BOARD_USES_QCNE),true)
 TARGET_LDPRELOAD := libNimsWrap.so
-endif
 endif
 
 -include vendor/oneplus/bacon/BoardConfigVendor.mk
